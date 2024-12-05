1441f3ba3c2b19a03157562c717daa6bfc528098
jhona166 <jdaltamiranoo@uce.edu.ec>
2024-12-04 19:50:08 -0500

    Tarea6React a

---
+++ js/react.js
@@ -0,0 +1,13 @@
+ReactDOM.render(
+    <div>
+        <h1>Hola Mundo</h1>
+        <p>Con React.JS</p>
+        <p> Cambios para reporte1</p>
+        <p> Cominezo de reporte1 </p>
+
+        <p> Cambios para reporte1</p>
+        <p> Cominezo de reporte1 </p>
+    </div>,
+    document.getElementById('myApp')
+);
+
